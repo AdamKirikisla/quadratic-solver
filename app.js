604275a44c3d4bf5c2a45792d056e8960edb2af5
Adam Kirikisla <280528387+AdamKirikisla@users.noreply.github.com>
2026-07-15 19:59:08 -0400

    add styles and fix some logic

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const form = document.querySelector("form");
 
 // Buttons
 const submitButton = document.querySelector('#submitButton');
+const clearButton = document.querySelector('#clearButton');
+
+// Clear button
+clearButton.addEventListener("click", () => {
+    document.querySelector("#result").textContent = "Solution:";
+    document.querySelector("#case").textContent = "Case";
+    document.querySelector("#d").textContent = "Discriminant";
+}
+);
 
 
 // Forms Event
@@ -19,6 +28,19 @@ form.addEventListener('submit', e => {
     const b = Number(constB.value)
     const c = Number(constC.value)
     console.log("Submitted");
+
+    // Declare information variables
+    const solution = document.querySelector('#result');
+    const discriminantText = document.querySelector('#d');
+    const caseText = document.querySelector('#case');
+
+    // a cannot be 0
+    if (a === 0) {
+        solution.textContent = "a cannot be 0 — this isn't a quadratic equation.";
+        caseText.textContent = "";
+        discriminantText.textContent = "";
+        return; // stops the rest of the function from running
+    }
 
     // Discriminant Logic
     const d = Math.pow(b, 2) - (4 * a * c);
@@ -52,22 +74,27 @@ form.addEventListener('submit', e => {
         return roots
     }
 
-    // Solution output
-    const solution = document.querySelector('#result');
+
 
     // Conditional for discriminant cases
     if (d > 0) {
         console.log(twoRealRoots(d));
         solution.textContent = 'x = ' + twoRealRoots(d)[0] + ', or x = ' + twoRealRoots(d)[1];
+        caseText.textContent = "Case: Two real roots ";
+        discriminantText.textContent = 'Discriminant = ' + d;
     }
     else if (d === 0) {
         console.log(repeatedRoots(d));
         solution.textContent = 'x = ' + repeatedRoots(d);
+        caseText.textContent = "Case: Repeated root ";
+        discriminantText.textContent = 'Discriminant = ' + d;
     }
 
     else if (d < 0) {
         console.log(twoImgRoots(d));
         solution.textContent = 'x = ' + twoImgRoots(d)[0] + ', or x = ' + twoImgRoots(d)[1];
+        caseText.textContent = "Case: Complex roots ";
+        discriminantText.textContent = 'Discriminant = ' + d;
     }
 
 
